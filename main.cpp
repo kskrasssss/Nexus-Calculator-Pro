1fe99b651b46ec54793c847777d4787d75ada9a8
Maria Vodko <163645840+MaykaXX@users.noreply.github.com>
2026-04-27 15:29:30 +0300

    Create Calculator.h and change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 
-class Calculator {
-public:
-    double add(double a, double b);
-    double subtract(double a, double b);
-    double multiply(double a, double b);
-    double divide(double a, double b);
-};
+#include "Calculator.h"
+
 
 int main() {
     std::cout << "--- Nexus Calculator v1.0 ---" << std::endl;
-    Calculator calc;
+    Calculator<int> calc;
 
 	// ... (Here you can add code to demonstrate the usage of the Calculator class)
 
