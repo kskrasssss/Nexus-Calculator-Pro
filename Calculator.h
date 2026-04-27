1ca2166d14cc0c99671ee14b3e2f51349a6b30c9
Yegor Datsiy <mrfrolix@gmail.com>
2026-04-27 15:42:14 +0300

    Add multiply and divide

---
+++ Calculator.h
@@ -0,0 +1,22 @@
+#ifndef UNTITLED7_CALCULATOR_H
+#define UNTITLED7_CALCULATOR_H
+
+template <typename T>
+class Calculator {
+public:
+
+    T add(T a, T b) {
+        return a + b;
+    };
+    T subtract(T a, T b);
+    T multiply(T a, T b) {
+        return a * b;
+    }
+    T divide(T a, T b){
+        return a / b;
+    }
+};
+
+
+
+#endif // UNTITLED7_CALCULATOR_H
