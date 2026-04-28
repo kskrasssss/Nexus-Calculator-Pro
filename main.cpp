564a2f797fabe75d84829b936f832e2071ddd1bd
kskrasssss <self.developmentkkras@gmail.com>
2026-04-28 11:50:51 +0300

    Refactor main function for calculator operations

--- main.cpp
+++ main.cpp
@@ -3,12 +3,44 @@
 #include "Calculator.h"
 
 
+template<typename T>
 int main() {
-    std::cout << "--- Nexus Calculator v1.0 ---" << std::endl;
+    std::cout << "--- Nexus Calculator ---" << std::endl;
     Calculator<int> calc;
-    int result = calc.add(1, 50);
-    std::cout << result << std::endl;
+    T n, m;
+    
+    int choice;
+    std::cout << "1. Addition\n";
+    std::cout << "2. Subtraction\n";
+    std::cout << "3. Multiplication\n";
+    std::cout << "4. Division\n";
 
+    switch (choice) {
+        case 1:
+            std::cout << "Enter two numbers: ";
+            std::cin >> n >> m;
+            std::cout << "Result: " << calc.add(n, m) << std::endl;
+            break;
+        case 2:
+            std::cout << "Enter two numbers: ";
+            std::cin >> n >> m;
+            std::cout << "Result: " << calc.subtract(n, m) << std::endl;
+            break;
+        case 3:
+            std::cout << "Enter two numbers: ";
+            std::cin >> n >> m;
+            std::cout << "Result: " << calc.multiply(n, m) << std::endl;
+            break;
+        case 4:
+            std::cout << "Enter two numbers: ";
+            std::cin >> n >> m;
+            std::cout << "Result: " << calc.division(n, m) << std::endl;
+            break;
+        default:
+            std::cout << "Invalid choice!" << std::endl;
+    }
+
+    
 	
 
     return 0;
